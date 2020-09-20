00d0fa6e22ca45043c1b927dcbce38b6fd51dc56
sunflo-h <sunflo0524@gmail.com>
2020-09-20 23:17:11 +0900

    9/20

--- JavaPrj/src/Practics/C5E2program.java
+++ JavaPrj/src/Practics/C5E2program.java
@@ -19,6 +19,7 @@ public class C5E2program {
 		Member[] mList = new Member[10];
 
 		// Member 객체 생성하고 데이터 입력 후 mList에 참조시켜서 10개의 공간에 넣기
+		// 데이터 입력
 		int countOfMember = 6;
 		{
 			int i = 0;
@@ -27,7 +28,7 @@ public class C5E2program {
 				String line = scan.nextLine();
 				String[] tokens = new String[countOfData];
 				tokens = line.split(",");
-				
+
 				Member m = new Member();
 				m.id = Integer.parseInt(tokens[0]);
 				m.uid = tokens[1];
@@ -36,23 +37,37 @@ public class C5E2program {
 
 				mList[i] = m;
 				i++;
+			}
+		}
 
+		// 이름을 기준으로 내림차순 버블정렬
+		for (int i = 0; i < countOfMember - 1; i++) {
+			for (int j = 0; j < countOfMember - 1 - i; j++) {
+				//화면 클리어
+				for (int a = 0; a < 50; a++) 
+					System.out.println();
+				//출력
+				for (int a = 0; a < countOfMember-1; a++) {
+					if (mList[a].age > mList[a+1].age) 
+						System.out.printf("*");
+					System.out.printf("%d,%5s,%s,%3d\n", mList[a].id, mList[a].uid, mList[a].name, mList[a].age);
+				}
+				Thread.sleep(1000);
+				if (mList[j].age > mList[j+1].age) {
+					Member temp = mList[j];
+					mList[j] = mList[j + 1];
+					mList[j + 1] = temp;
+					
+				}
 			}
 		}
-		
-		//이름을 기준으로 내림차순 버블정렬
-		for(int i=0;i<countOfMember-1;i++) {
-			for(int j=0;j<countOfMember-1-i;j++) {
-				
-			}
-		}
+
 		// 출력 -----
-		for (int i = 0; i < countOfMember; i++) {
-			Member m = new Member();
-			m = mList[i];
-			System.out.printf("%d,%s,%s,%d\n",m.id,m.uid,m.name,m.age);
-		}
+//		for (int i = 0; i < countOfMember; i++) {
+//			Member m = new Member();
+//			m = mList[i];
+//			System.out.printf("%d,%s,%s,%d\n", m.id, m.uid, m.name, m.age);
+//		}
 		System.out.println("작업완료");
 	}
-
 }
